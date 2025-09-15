d7bec4c060e26fc4764d0ffbc423ec2fe55fe24f
Malakai <poncemalakai@gmail.com>
2025-09-14 20:42:49 -0700

    Commit 2 - reverse the two until the left pointer meets or crosses teh right

--- Lab_7.cpp
+++ Lab_7.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 const int N = 5; // size of array
+
+string* reverseArray(string* a, int n) {
+    string* l = a;                          // left pointer - first one
+    string* r = a + n - 1;                  // right pointer - last one
+    while (l < r) {
+        swap(*l, *r);                       // swap values at left and right pointers   
+        l++;                                // movign inward  on both ends 
+        r--;
+    }
+    return a;
+}
 
 
